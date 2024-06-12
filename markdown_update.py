db13d48704e620f8d1f88cdba79ce40580c0d65c
Gareth Flandro <137940955+divnerd@users.noreply.github.com>
2024-06-12 17:32:00 -0400

    Delete markdown_update.py

--- markdown_update.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-target_date = datetime.date(2023, 10, 27)
-days_since = (datetime.date.today() - target_date).days
-
-with open('README.md', 'a') as f:
-    f.write(f"<p>I'm <strong>divnerd</strong> - a backend developer, python lover, and high-schooler. I'm in my school's cybersecurity club and I've been programming for {days_since} days!</p>\n")
