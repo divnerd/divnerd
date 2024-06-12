1b1fdb259dd2ccbbddcbc26fe59361cf2b31502a
Gareth Flandro <137940955+divnerd@users.noreply.github.com>
2024-06-12 15:09:27 -0400

    Update markdown_update.py

--- markdown_update.py
+++ markdown_update.py
@@ -5,4 +5,4 @@ target_date = datetime.date(2023, 10, 27)
 days_since = (datetime.date.today() - target_date).days
 
 with open('markdown_output.md', 'w') as f:
-    f.write(f"<p>Days since {target_date.strftime('%Y-%m-%d')}: {days_since}</p>\n")
+    f.write(f"<p>I'm <strong>divnerd</strong> - a backend developer, python lover, and high-schooler. I'm in my school's cybersecurity club and I've been programming for {days_since} days!</p>\n")
