bfdc883e3201552e92b34393b474d8cb81a9ada5
Gareth Flandro <137940955+divnerd@users.noreply.github.com>
2024-06-12 15:13:22 -0400

    Update markdown_update.py

--- markdown_update.py
+++ markdown_update.py
@@ -1,8 +1,7 @@
 import datetime
 
 target_date = datetime.date(2023, 10, 27)
-
 days_since = (datetime.date.today() - target_date).days
 
-with open('markdown_output.md', 'w') as f:
+with open('README.md', 'a') as f:
     f.write(f"<p>I'm <strong>divnerd</strong> - a backend developer, python lover, and high-schooler. I'm in my school's cybersecurity club and I've been programming for {days_since} days!</p>\n")
